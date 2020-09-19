9a6b461a7423ccd41550a9fbe7e925daf162e100
Leah Riffell <leah.riffell@gmail.com>
2020-09-19 10:19:41 -0600

    Add discounts to seeds and destroy all before reseeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+OrderItem.destroy_all
+Item.destroy_all
+Discount.destroy_all
+Merchant.destroy_all
+
 # merchants
-monster_shop = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+monster_shop = Merchant.create!(name: 'Megans Monsters', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
 pet_shop = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
 
 # monster_shop's items
@@ -16,3 +21,8 @@ monster_shop.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50
 
 # pet_shop's items
 pet_shop.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+
+# discounts
+monster_shop.discounts.create!(percent: 10, min_qty: 2)
+monster_shop.discounts.create!(percent: 10, min_qty: 10)
+monster_shop.discounts.create!(percent: 20, min_qty: 20)
