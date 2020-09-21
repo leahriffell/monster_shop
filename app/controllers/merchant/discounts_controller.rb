0499605746e9d4acc4b4dc735d579b182268e68c
Leah Riffell <leah.riffell@gmail.com>
2020-09-21 17:18:08 -0600

    Add merchant discounts controller

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,4 +1,17 @@
 class Merchant::DiscountsController < Merchant::BaseController
   def new
+    @merchant = Merchant.find(current_user.merchant_id)
+    @discount = Discount.new
+  end
+
+  def create
+    discount = Merchant.find(params[:format]).discounts.create(discount_params)
+    redirect_to merchant_dashboard_path
+  end
+
+  private 
+
+  def discount_params
+    params[:discount].permit(:percent, :min_qty)
   end
 end
