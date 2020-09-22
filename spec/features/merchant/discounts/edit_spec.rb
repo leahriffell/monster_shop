a58dd7822e6df25d34cfc78461bbc5eb4214308b
Leah Riffell <leah.riffell@gmail.com>
2020-09-22 09:25:47 -0600

    Add test that merch employee can edit a discount

---
+++ spec/features/merchant/discounts/edit_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Discount Edit Page' do
+  describe 'As an employee of a merchant' do
+    before :each do
+      @merchant = create(:merchant)
+
+      @m_user = FactoryBot.create(:user, email: 'merchant@merchant.com', password: 'password', role: 1)
+      @merchant.users << @m_user
+
+      @discount = create(:discount, merchant: @merchant)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
+    end
+
+    it "I can see existing discount information pre-populated" do 
+      visit "/merchant/discounts/#{@discount.id}/edit"
+
+      expect(find_field(:discount_percent).value).to eq("#{@discount.percent}")
+      expect(find_field(:discount_min_qty).value).to eq("#{@discount.min_qty}")
+    end
+    
+    it "I can edit existing info" do 
+      visit "/merchant/discounts/#{@discount.id}/edit"
+
+      fill_in :discount_percent, with: 7
+      fill_in :discount_min_qty, with: 7
+      click_button "Update Discount"
+
+      visit '/merchant'
+
+      within("#discount-#{@discount.id}") do
+        expect(page).to have_content("Discount percent: 7%")
+        expect(page).to have_content("Minimum quantity: 7")
+      end
+    end
+  end
+end
