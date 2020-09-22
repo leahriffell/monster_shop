d2a46485bd001a06365b3fe7bd621261458e450d
Leah Riffell <leah.riffell@gmail.com>
2020-09-22 11:16:15 -0600

    Update discounts factory to produce floats vs. ints

--- spec/factories/discounts.rb
+++ spec/factories/discounts.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :discount do
-    percent { Faker::Number.within(range: 1..99) }
+    percent { Faker::Number.within(range: 1..99)*0.01 }
     min_qty { Faker::Number.within(range: 1..50) }
     association :merchant
   end
