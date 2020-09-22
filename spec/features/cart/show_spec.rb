3b0de6e6702e42fe105ef258e254356bf29834bc
Leah Riffell <leah.riffell@gmail.com>
2020-09-22 11:41:23 -0600

    Move discount related tests to their own describe block in cart show

--- spec/features/cart/show_spec.rb
+++ spec/features/cart/show_spec.rb
@@ -11,11 +11,6 @@ RSpec.describe 'Cart Show Page' do
       @giant = @monster_shop.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
 
       @hippo = @pet_shop.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
-
-      @monster_shop.discounts.create!(percent: 0.1, min_qty: 2)
-      @monster_shop.discounts.create!(percent: 0.1, min_qty: 10)
-      @monster_shop.discounts.create!(percent: 0.15, min_qty: 8)
-      @monster_shop.discounts.create!(percent: 0.2, min_qty: 20)
     end
 
     describe 'I can see my cart' do
@@ -57,33 +52,6 @@ RSpec.describe 'Cart Show Page' do
 
         expect(page).to have_content('Your Cart is Empty!')
         expect(page).to_not have_button('Empty Cart')
-      end
-
-      it "I can view any discounts on items" do
-        @cart = Cart.new({
-          @ogre.id.to_s => 1,
-          @giant.id.to_s => 10,
-          @hippo.id.to_s => 50
-          })
-        allow_any_instance_of(ApplicationController).to receive(:cart).and_return(@cart)
-
-        visit '/cart'
-
-        expect(@cart.discount_percent_for(@ogre)).to eq(nil)
-        expect(@cart.discount_percent_for(@giant)).to eq(0.15)
-        expect(@cart.discount_percent_for(@hippo)).to eq(nil)
-
-        within("#item-#{@ogre.id}") do 
-          expect(page).to have_content("Price: #{number_to_currency(@ogre.price)}")
-          expect(page).to have_content("Subtotal: #{number_to_currency(@cart.subtotal_of(@ogre.id))}")
-        end
-
-        within("#item-#{@giant.id}") do 
-          expect(page).to have_content("Discounted price: #{number_to_currency(@giant.price * (1 - @cart.discount_percent_for(@giant)))}")
-          expect(page).to have_content("Subtotal: #{number_to_currency(@cart.count_of(@giant.id) * @cart.discount_price_for(@giant))}")
-        end
-
-        expect(page).to have_content("Total: #{number_to_currency((@ogre.price * 1) + (42.5 * 10) + (@hippo.price * 50))}")
       end
     end
 
@@ -200,6 +168,42 @@ RSpec.describe 'Cart Show Page' do
         expect(current_path).to eq('/cart')
         expect(page).to_not have_content("#{@hippo.name}")
         expect(page).to have_content("Cart: 0")
+      end
+    end
+
+    describe 'I can view discounts in cart' do
+      before :each do 
+        @monster_shop.discounts.create!(percent: 0.1, min_qty: 2)
+        @monster_shop.discounts.create!(percent: 0.1, min_qty: 10)
+        @monster_shop.discounts.create!(percent: 0.15, min_qty: 8)
+        @monster_shop.discounts.create!(percent: 0.2, min_qty: 20)
+        @pet_shop.discounts.create!(percent: 0.2, min_qty: 50)
+
+        # ogre and giant are monster shop
+        # hippo is pet_shop
+      end
+
+      it 'I can view any existing discounts on items reflected in subtotals and total'do
+        @cart = Cart.new({
+          @ogre.id.to_s => 1,
+          @giant.id.to_s => 10,
+          })
+
+        allow_any_instance_of(ApplicationController).to receive(:cart).and_return(@cart)
+
+        visit '/cart'
+
+        within("#item-#{@ogre.id}") do 
+          expect(page).to have_content("Price: #{number_to_currency(@ogre.price)}")
+          expect(page).to have_content("Subtotal: #{number_to_currency(@cart.subtotal_of(@ogre.id))}")
+        end
+
+        within("#item-#{@giant.id}") do 
+          expect(page).to have_content("Discounted price: #{number_to_currency(@giant.price * (1 - @cart.discount_percent_for(@giant)))}")
+          expect(page).to have_content("Subtotal: #{number_to_currency(@cart.count_of(@giant.id) * @cart.discount_price_for(@giant))}")
+        end
+
+        expect(page).to have_content("Total: #{number_to_currency((@ogre.price * 1) + (42.5 * 10))}")
       end
     end
   end
